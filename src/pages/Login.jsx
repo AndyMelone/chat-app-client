22c46c9c537ac5a906433ee8692e110a8f03b34a
AndyMelone <andy.30@yahoo.com>
2023-11-08 19:56:04 +0000

    maj login&register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,34 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Login = () => {
-    return ( <>login </> );
-}
- 
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            justifyContent: "center",
+            paddingTop: "10%",
+            height: "100vh",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2> Login</h2>
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button varaiant="primary" type="submit">
+                Login
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,35 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-    return ( <>register</> );
-}
- 
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            justifyContent: "center",
+            paddingTop: "10%",
+            height: "100vh",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2> Register</h2>
+              <Form.Control type="text" placeholder="name" />
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button varaiant="primary" type="submit">
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
+
 export default Register;
