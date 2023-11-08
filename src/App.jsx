70ecd281a34f6903817cfef08d041027352fc566
AndyMelone <andy.30@yahoo.com>
2023-11-08 16:56:20 +0000

    Sauvegarde des modifications non suivies

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css' 
+import { Routes, Route , Navigate } from "react-router-dom"
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Chat from './pages/Chat'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
